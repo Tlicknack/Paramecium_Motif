e965723adb88e67bc9afa8311146539d1938d49c
Tlicknack <35544671+Tlicknack@users.noreply.github.com>
2018-05-11 10:22:24 -0700

    Add files via upload
    Final version of program to return fasta file of DNA upstream of para-orthologs in Paramecium. Must be modified for other species

--- Paramecium_Cut_Upstream_new_algorithm.R
+++ Paramecium_Cut_Upstream_new_algorithm.R
@@ -1,159 +1,135 @@
-extractUpstream = function(sp, geneID, lGFF, lFasta){                          #make gff and fasta into list with id as sp
+#This program will group together the DNA sequence upstream of ortho-parologs and output them into a fasta file. This file can then be used for motif searching (e.g. MEME)
+#The program requires:
+  #FASTA files for each species 
+  #modified GFFs which contains only the scaffold/chromosome number, gene start, gene end, strand, and geneID for each species
+  #POFF tables of orthoparalogs which contains number of species,number of genes, Alg.-Conn, and a variable number of species names under which geneIDs will be located
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("seqinr")
+#install.packages("stringi")
+library("seqinr")
 
-  
+
+extractUpstream = function(sp, geneID, lGFF, lfasta, fname){                          #Pass species name, geneID, list of GFFs and list of FASTAs for that family
+
+  grep_geneID = which(lGFF[[sp]]$V5 == geneID, arr.ind=TRUE)
+  cat("Grep= ", grep_geneID, "\n")
+  #grep_geneID = as.integer(grep(geneID, lGFF[[sp]][,5]))                             #search each gff for geneID, return column number
+  row_geneID = lGFF[[sp]][grep_geneID, ]
+  if(row_geneID[,4] == "+"){                                      #get Gene's starting position, depending upon its strand 
+    gene_start = row_geneID[,2]   
+  } else{
+    gene_start = row_geneID[,3]
+  }
+  scaf = row_geneID[,1]                                             #find the scaffold holding the gene 
+  cat("scaf= ", scaf, ".....", "gene start= ", gene_start, ".....", "strand= ", row_geneID[,4], "\n")
+  scafSeq=as.character(lfasta[[sp]][scaf])
+  if(row_geneID[,4] == "+"){                                       #extract the correct upstream sequence. if +, then its just 200bp from start of gene, if -, then its 200 from end of gene
+    upstream = substr(scafSeq, gene_start, gene_start-200)
+  } else {
+    upstream = substr(scafSeq, gene_start, gene_start+200)
+    upstream = c2s(rev(comp(s2c(upstream))))
+  }
+  return(upstream)
+  #cat("Upstream sequence for ", geneID, "has been done successfully. \n")
 }
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-
-getPOFFid = function(vsp, POFF) {                                     #Pass POFF and list of species names
-
+getPOFFid = function(vsp, POFF, lGFF, lFasta) {                                     #Pass POFF and list of species names
+  
   i = 1                                                               #counter for iterating through poff
   for(i in 1:nrow(POFF)) {                                            #iterate through POFF
-    cat("Parsing line ", i, "\n") 
+    fname = paste(names(POFF[4]), "_", i, ".fasta", sep="")       #file name that resets each row
+    cat("Parsing line ", i, "\n")           #fun printed message
+    
+    vupstream = c()
     for(sp in vsp) {                                                  #iterate through species names, which are identifiers
       sp_family = as.character(POFF[i,sp])                            #get proteinIDs from each column in each row
       vsplit = strsplit(sp_family, ",")[[1]]                          #split each proteinID as it comes out, some will have commas, some won't. Will do this for each species as we iterate
       
-      for(geneID in vsplit){                                          #iterate through
-        cat("Now extracting sequence upstream of ", geneID, " (which is in", sp, ")\n")
-        extractUpstream(sp, geneID, lGFF, lFasta)                     #call function that extracts sequence given species name, the proteinID, a list of GFFs, and a list of FASTAs
+      for(geneID in vsplit){                                          #iterate through 1 or 2 proteinIDs (depending on if they needed to be split)
+
+        if(geneID != "*"){
+          cat("Now extracting sequence upstream of ", geneID, " (which is in", sp, ")\n")                     
+          upstreamSeq = extractUpstream(sp, geneID, lGFF, lFasta, fname)
+          cat("Sequence extracted: '", upstreamSeq, "'\n")
+          vupstream[geneID] = upstreamSeq                     #changed from append to set each position, will be able to iterate through names in writeVectorAsFasta fxn
+        }
       }
     }
-      cat("line", i, "done.\n-----------------------\n")
+    writeVectorAsFasta(vupstream, fname)
+    #cat("line", i, "done.\n-----------------------\n")      #print that line one is complete
   }
 }
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 writeVectorAsFasta <- function(vseq, fname){                          #Pass list of sequences from extractUpstream and name of file. This will write to fasta file as output
-  cat("", file=fname) # Just to reset the file to empty if it already exists
+  #cat("FNAME: ", fname, "\n")
+  #cat("", file=fname)                                         # Just to reset the file to empty if it already exists
+  print(vseq)
+  j = 0
   for(id in names(vseq)){
-    cat(">", id, "\n", seq, "\n", sep="", file=fname, append=T)       #dirty fasta format. will write to current directory
+    j = j + 1
+    #cat("Writing ", id ," to file '", fname, "'")
+    cat(">", id, "\n", vseq[[j]], "\n", sep="", file=fname, append=T)       #dirty fasta format. will write to current directory
   }
 }
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+#MAIN:                             #WD= Desktop/Paramecium_Genome_Data/Paramecium_Upstream_FASTA. ALl other files are ../Paramecium_whatever
 
-main = function(){
+  bi_poff = read.table("../Paramecium_POFF/head_bi_poff", header=TRUE, sep="\t", comment.char="$")  #read POFFs, change comment character so first line is read as header
+  tet_poff = read.table("../Paramecium_POFF/head_tet_poff", header=TRUE, sep="\t", comment.char="$")
+  sex_poff = read.table("../Paramecium_POFF/head_sex_poff", header=TRUE, sep="\t", comment.char="$")
+  colnames(bi_poff) = gsub(".faa", "", colnames(bi_poff))     #fix column names
+  colnames(tet_poff) = gsub(".faa", "", colnames(tet_poff)) 
+  colnames(sex_poff) = gsub(".faa", "", colnames(sex_poff)) 
   
-  bi_poff = read.table("~/Paramecium_POFF/biaurelia_group.poff", header=TRUE, sep="\t", comment.char="$")  #read POFFs, change comment character so first line is read as header
-  tet_poff = read.table("~/Paramecium_POFF/tetraurelia_group.poff", header=TRUE, sep="\t", comment.char="$")
-  sex_poff = read.table("~/Paramecium_POFF/sexaurelia_group.poff", header=TRUE, sep="\t", comment.char="$")
+  vsp_bi = colnames(bi_poff)[4:ncol(bi_poff)]                 #get list of spcies names for each family... just in case
+  vsp_tet = colnames(tet_poff)[4:ncol(tet_poff)] 
+  vsp_sex = colnames(sex_poff)[4:ncol(sex_poff)] 
+#----------------------------------------
+  lbi_GFF = list()
+  lsex_GFF = list()
+  ltet_GFF = list()
   
-  for(poff in POFF){                                        #get list of species names
-    colnames(poff) = gsub(".faa", "", colnames(poff))                   #modify column names to remove .faa .... CHANGE FOR DIFFERENT COLUMN NAMES
-    vsp = colnames(poff)[4:ncol(poff)]                                  #capture proteinIDs in each line using column name as identifier
+  for(sp_bi in vsp_bi){
+    bi_gff_name = paste("../Paramecium_GFF/", sp_bi, "-gene.tab", sep="")             #name of GFFs all look the same, so use list of names to create file names
+    bi_gff_load = read.table(bi_gff_name, sep="\t")                       #load each file given its name
+    lbi_GFF[[sp_bi]] = bi_gff_load                                         #add each file to list of GFFs with name of species as identifier
   }
-  #----------------------------------------
-  
-  lGFF = list()                                                         #Make list of GFFs. 
-  for(sp in vsp){                                     
-    gff_name = paste("~/", sp, "-gene.tab", sep="")             #name of GFFs all look the same, so use list of names to create file names
-    gff_load = read.table(gff_name, sep="\t")                   #load each file given its name
-    lGFF[sp] = gff_load                                         #add each file to list of GFFs with name of species as identifier
+  for(sp_tet in vsp_tet){
+    tet_gff_name = paste("../Paramecium_GFF/", sp_tet, "-gene.tab", sep="") 
+    tet_gff_load = read.table(tet_gff_name, sep="\t")
+    ltet_GFF[[sp_tet]] = tet_gff_load
   }
-  bi_GFF = lGFF["pbi", "ppent", "pprim", "pquad", "ptre", "pnov"]
-  sex_GFF = lGFF["psex", "pjen", "pson"]
-  tet_GFF = lGFF["ptet", "psept", "poct", "pdodec", "pdec"]
-  #---------------------------------------
-  
-  lFASTA = list()                                                             #Make list of FASTA. Must manually plug in each due to different file names
-  lFASTA["pbi"] = "biaurelia_V1-4_assembly_v1.fa"
-  lFASTA["pdec"] = "pdecaurelia_mac_223_v1.0.fa" 
-  lFASTA["dodec"] = "pdodecaurelia_mac_274_v1.0.fa" 
-  lFASTA["pjen"] = "pjenningsi_mac_M_v1.0.fa"
-  lFASTA["pnov"] = "pnovaurelia_mac_TE_v1.0.fa"
-  lFASTA["poct"] = "poctaurelia.fasta"
-  lFASTA["ppent"] = "ppentaurelia_mac_87_v1.0.fa"   
-  lFASTA["pprim"] = "primaurelia_Ir4-2_assembly_v1.fasta"
-  lFASTA["pquad"] = "pquadecaurelia_mac_NiA_v1.0.fa"
-  lFASTA["psept"] = "pseptaurelia_mac_38_v1.0.fa"
-  lFASTA["pson"] = "psonneborni_mac_ATCC_30995_v1.0.fa"
-  lFASTA["ptre"] = "ptredecaurelia_209_AP38_filtered.fa"
-  lFASTA["ptet"] = "ptetraurelia_mac_51.fa"
-  
-  bi_FASTA = list()
-  for(bi_f in lFASTA["pbi", "ppent", "pprim", "pquad", "ptre", "pnov"]){
-    for(sp in vsp){
-      bi_FASTA[sp] = bi_f
-    }
+  for(sp_sex in vsp_sex){
+    sex_gff_name = paste("../Paramecium_GFF/", sp_tet, "-gene.tab", sep="") 
+    sex_gff_load = read.table(sex_gff_name, sep="\t")
+    ltet_GFF[[sp_sex]] = sex_gff_load    
   }
-  sex_FASTA = list()
-  for(sex_f in lFASTA["psex", "pjen", "pson"]){
-    sex_FASTA = append(sex_FASTA, sex_g)
-  }
-  tet_FASTA = list()
-  for(tet_f in lFASTA["ptet", "psept", "poct", "pdodec", "pdec"]){
-    tet_FASTA = append(tet_FASTA, tet_g)
-  }
-}  
-#---------------------------------------------------------------------------------------------------------------------------------------  
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#---------------------------------------
+  lbi_FASTA = list()
+  lbi_FASTA[["pbi"]] = read.fasta("../Paramecium_FASTA/biaurelia_V1-4_assembly_v1.fa", as.string=TRUE)
+  lbi_FASTA[["ppent"]] = read.fasta("../Paramecium_FASTA/ppentaurelia_mac_87_v1.0.fa", as.string=TRUE)
+  lbi_FASTA[["pprim"]] = read.fasta("../Paramecium_FASTA/primaurelia_Ir4-2_assembly_v1.fasta", as.string=TRUE)
+  lbi_FASTA[["pquad"]] = read.fasta("../Paramecium_FASTA/pquadecaurelia_mac_NiA_v1.0.fa", as.string=TRUE)
+  lbi_FASTA[["ptre"]] = read.fasta("../Paramecium_FASTA/ptredecaurelia_209_AP38_filtered.fa", as.string=TRUE)
+  lbi_FASTA[["pnov"]] = read.fasta("../Paramecium_FASTA/pnovaurelia_mac_TE_v1.0.fa", as.string=TRUE)
+  getPOFFid(vsp_bi, bi_poff, lbi_GFF, lbi_FASTA)
+  lsex_FASTA = list()
+  lsex_FASTA[["psex"]] = read.fasta("../Paramecium_FASTA/sexaurelia_AZ8-4_assembly_v1.fasta", as.string=TRUE)
+  lsex_FASTA[["pjen"]] = read.fasta("../Paramecium_FASTA/pjenningsi_mac_M_v1.0.fa", as.string=TRUE)
+  lsex_FASTA[["pson"]] = read.fasta("../Paramecium_FASTA/psonneborni_mac_ATCC_30995_v1.0.fa", as.string=TRUE)
+  getPOFFid(vsp_sex, sex_poff, lsex_GFF, lsex_FASTA)
+  ltet_FASTA = list()
+  ltet_FASTA[["pdec"]] = read.fasta("../Paramecium_FASTA/pdecaurelia_mac_223_v1.0.fa", as.string=TRUE)
+  ltet_FASTA[["pdodec"]] = read.fasta("../Paramecium_FASTA/pdodecaurelia_mac_274_v1.0.fa", as.string=TRUE)
+  ltet_FASTA[["poct"]] = read.fasta("../Paramecium_FASTA/poctaurelia.fasta", as.string=TRUE)
+  ltet_FASTA[["psept"]] =read.fasta("../Paramecium_FASTA/pseptaurelia_mac_38_v1.0.fa", as.string=TRUE)
+  ltet_FASTA[["ptet"]] = read.fasta("../Paramecium_FASTA/ptetraurelia_mac_51.fa", as.string=TRUE)
+  getPOFFid(vsp_tet, tet_poff, ltet_GFF, ltet_FASTA)
   
-pbi_gff = read.table("~/Paramecium_GFF/pbi-gene.tab", sep="\t")
-pdec_gff = read.table("pdec-gene.tab", sep="\t")
-pdodec_gff = read.table("pdodec-gene.tab", sep="\t")
-pjen_gff = read.table("pjen-gene.tab", sep="\t")
-pnov_gff = read.table("pnov-gene.tab", sep="\t")
-poct_gff = read.table("poct-gene.tab", sep="\t")
-ppent_gff = read.table("ppent-gene.tab", sep="\t")
-pprim_gff = read.table("pprim-gene.tab", sep="\t") 
-pquad_gff = read.table("pquad-gene.tab", sep="\t")
-psept_gff = read.table("psept-gene.tab", sep="\t")
-psex_gff = read.table("psex-gene.tab", sep="\t")
-pson_gff = read.table("pson-gene.tab", sep="\t")
-ptet_gff = read.table("ptet-gene.tab", sep="\t")
-ptre_gff = read.table("ptre-gene.tab", sep="\t")
-
-pbi_fa = read.fasta("~/Paramecium_FASTA/biaurelia_V1-4_assembly_v1.fa", as.string=TRUE)
-pdec_fa = read.fasta("pdecaurelia_mac_223_v1.0.fa", as.string=TRUE)
-pdodec_fa = read.fasta("pdodecaurelia_mac_274_v1.0.fa", as.string=TRUE)
-pjen_fa = read.fasta("pjenningsi_mac_M_v1.0.fa", as.string=TRUE)
-pnov_fa = read.fasta("pnovaurelia_mac_TE_v1.0.fa", as.string=TRUE)
-poct_fa = read.fasta("poctaurelia.fasta", as.string=TRUE)
-ppent_fa = read.fasta("ppentaurelia_mac_87_v1.0.fa", as.string=TRUE)
-pprim_fa = read.fasta("primaurelia_Ir4-2_assembly_v1.fasta", as.string=TRUE)
-pquad_fa = read.fasta("pquadecaurelia_mac_NiA_v1.0.fa", as.string=TRUE)
-psept_fa =read.fasta("pseptaurelia_mac_38_v1.0.fa", as.string=TRUE)
-psex_fa = read.fasta("sexaurelia_AZ8-4_assembly_v1.fasta", as.string=TRUE)
-pson_fa = read.fasta("psonneborni_mac_ATCC_30995_v1.0.fa", as.string=TRUE)
-ptet_fa = read.fasta("ptetraurelia_mac_51.fa", as.string=TRUE)
-ptre_fa = read.fasta("ptredecaurelia_209_AP38_filtered.fa", as.string=TRUE)
-
-
-pbi_gff = "~/Paramecium_GFF/pbi-gene.tab"
-pdec_gff = "~/pdec-gene.tab"
-pdodec_gff = "~/pdodec-gene.tab "
-pjen_gff = "~/pjen-gene.tab "
-pnov_gff = "~/pnov-gene.tab "
-poct_gff = "~/poct-gene.tab "
-ppent_gff = "~/ppent-gene.tab "
-pprim_gff = "~/pprim-gene.tab "
-pquad_gff = "~/pquad-gene.tab "
-psept_gff = "~/psept-gene.tab "
-psex_gff = "~/psex-gene.tab "
-pson_gff = "~/pson-gene.tab "
-ptet_gff = "~/ptet-gene.tab "
-ptre_gff = "~/ptre-gene.tab "
-lGFF = c(pbi_gff, pdec_gff, pdodec_gff, pjen_gff, pnov_gff, poct_gff, ppent_gff, pprim_gff, pquad_gff, psept_gff, psex_gff, pson_gff, ptet_gff, ptre_gff)
-names(lGFF) = c("pbi", "pdec", "pdodec", "pjen", "pnov", "poct", "ppent", "pprim", "pquad", "psept", "psex", "pson", "ptet", "ptre")
-
-
-
-
-
-
-
