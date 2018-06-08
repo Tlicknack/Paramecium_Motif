25ced29aa17f69d1a63e50b8be102e3de46c552f
Tlicknack <35544671+Tlicknack@users.noreply.github.com>
2018-06-08 15:27:37 -0700

    Given a list of motifs and an intergenic fasta file, this will create a table that has every motif + surrounding sequence within intergenic

---
+++ Get_Motif_Upstream.R
@@ -0,0 +1,93 @@
+#This script will: 
+  #look for DNA motifs within the intergenic region of a genome of interest 
+  #extract all upstream nt's to the start codon and downstream until the whole sequence == 150nts
+  #create a fasta file of upstream-motif sequences for all instances of that motif
+#INPUT: 
+  #Fasta file of motifs with some ID
+  #Fasta file of intergenic sequence from some organism of interest .... see Make_Intergenic_FASTA.R
+  #Gff-tab file of intergenic DNA for species X .... See R script to produce this file
+    #Layout- scafold_number   intergenic_start    intergenic_end    geneID_5+     geneID_3+     geneID_5-     geneID_3-
+#OUTPUT:
+  #Fasta file for each motif of all upstream sequences
+
+
+#in first function, use position to extract sequence around motif (100 on each side)
+  #if on - strand, extract other side and make reverse complement
+
+
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("seqinr")
+library("seqinr")
+
+getIntergenic = function(fasta, vpos, size){
+  vsequences = c()                                                        #initiate vector of sequences
+  for(l in 1:length(vpos)){                                               #iterate through vector of positions of motif
+    sequence = substr(fasta[1], (vpos[l]-(size/2)), (vpos[l]+(size/2)))      #extract surrounding region of motif
+    vsequences = append(vsequences, sequence)                             #add it to vector of sequences
+  }
+  return(vsequences)
+}
+
+
+findMotifs <- function(motif, seq){
+  vpos = c()                    #initiate vector of position
+  res = gregexpr(motif, seq)    #get coordinates of regular expression match ... will match multiple times per sequence
+  v = unlist(res)
+  if(v[1] > (-1)){          #gregexpr returns -1 if it doesnt find motif in seq
+    vpos = v                        #vector of positions
+  }
+  reverse_motif = c2s(rev(comp(s2c(motif))))      #get reverse complementary sequence... convert string to list of char, then do comp and reverse, then convert back to string
+  rev_res = gregexpr(reverse_motif, seq)
+  rev_v = unlist(rev_res)
+  if(rev_v[1] > (-1)){
+    vpos = append(vpos, rev_v)
+  }
+  return(vpos)
+}
+
+
+findIntergenic = function(intergenic_fasta, lmotifs, intergenic_size){ #take as an input the intergenic fasta from makeIntergenicFasta, and a list of motifs uploaded with seqinr's read.alignment
+  total_counter = 0
+  x = c("Motif", "Sequence", "Scaffold_Genes", "Position")              #Column names of final data frame
+  final_seq_file = matrix(ncol=4)
+  colnames(final_seq_file) = x
+  for(i in 1:lmotifs[[1]]){                                             #iterate through fasta of motifs [[1]] which is number of sequences
+    seq_counter = 0                                             #init a counter of sequences for each motif
+    motif = as.character(lmotifs[[3]][i])                                             #sequence of i'th motif
+    seq_count = length(intergenic_fasta)                                #get length of intergenic fasta file
+    for(sequ in 1:seq_count){                                           #interate through length of sequences in intergenic fasta
+      inter_seq = as.character(intergenic_fasta[[sequ]][1])                 #save the sequence of the current intergenic region
+      vPosOfMotifs = findMotifs(motif, inter_seq)                         #RETURNS VECTOR OF POSITION OF MOTIFS
+      if(length(vPosOfMotifs) > 0){
+        v_extracted_seq = getIntergenic(intergenic_fasta[[sequ]], vPosOfMotifs, intergenic_size) #RETURNS VECTOR OF SEQUENCES AROUND MOTIF
+      } else{
+        v_extracted_seq = NULL
+      }
+      if(length(v_extracted_seq > 1)){                      #Only add to final table if extraction worked. without this, you get error
+          for(ex_seq in 1:length(v_extracted_seq)){         #iterate through number of extracted sequences (should be equal to vpos)
+            seq_counter = seq_counter + 1                   #add 1 to the sequence counter so each new sequence is added to the next row in the temp table
+
+            final_seq_file[(seq_counter+total_counter),1] = motif                                       #add values to the final output table 
+            final_seq_file[(seq_counter+total_counter),2] = v_extracted_seq[ex_seq]
+            final_seq_file[(seq_counter+total_counter),3] = getAnnot(intergenic_fasta[[sequ]])
+            final_seq_file[(seq_counter+total_counter),4] = vPosOfMotifs[ex_seq]
+            tmp_row = c(1,2,3,4)                                                                    #create a temp row 
+            final_seq_file = rbind(final_seq_file, tmp_row)                                         #bind the temp row to the existing table ... without this, we get out of bounds script
+        }
+      }         #RETURNS GIANT CHARACTER NOT A MATRIX!!
+    }
+    total_counter = total_counter + seq_counter
+  }
+  #exportSeq(final_seq_file)
+  return(final_seq_file)
+}
+
+
+#MAIN
+lmotifs = read.alignment("/home/tlicknac/Desktop/Paramecium_Genome_Data/Jeff_motifs.fa", format="fasta", forceToLower = TRUE)               #fasta of motifs                        #CHANGE                                                                                           #CHANGE
+#lmotifs = read.alignment("/home/tlicknac/Desktop/Paramecium_Genome_Data/tatabox.fa", format="fasta", forceToLower = TRUE) 
+intergenic_size = 200
+intergenic_fasta = read.fasta("/home/tlicknac/Desktop/Paramecium_Genome_Data/Paramecium_FASTA/ptet-intergenic.fasta", as.string=TRUE)
+intergenic_seq = findIntergenic(intergenic_fasta, lmotifs, intergenic_size)                                                                   #save table as variable 
+#write.table(intergenic_seq, file="Tata_Taattaa_surrounding_seqs.csv", sep=",")                                                                #make a file, HARDCODE
+write.table(intergenic_seq, file="Jeff_Motifs_surrounding_seqs.csv", sep=",")                                                                #make a file, HARDCODE
