eeb682c9ac6e1b157a5618242582480ae0bcb1d8
Tlicknack <35544671+Tlicknack@users.noreply.github.com>
2018-08-24 10:37:30 -0700

    old bash script used to convert full GFFs to genic GFFs

---
+++ GFF_modify_genic.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Iterate through all Paramecium .gff's to create new table with just genes
+
+parameciumgff=*.gff
+
+for species in $parameciumgff
+do
+        bn=`basename $species -full.gff`
+        tableFile=$bn-gene.tab
+        echo "$species -> $bn -> $tableFile"
+        grep -P "\tgene\t" $species | cut -f 1,4,5,7,9 | cut -d ';' -f 1 > $tableFile
+        sed -i 's/ID=//' $tableFile
+       cat $species | grep "   gene    " | cut -f 1,4,5,6,9 > {$species}_table
+done
