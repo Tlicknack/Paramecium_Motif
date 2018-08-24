b57327f0389de40f8d22d393b46c754729ae79d5
Tlicknack <35544671+Tlicknack@users.noreply.github.com>
2018-08-24 11:03:09 -0700

    changed for all aurelias not 3 families

--- meme_auto_query.py
+++ meme_auto_query.py
@@ -3,19 +3,17 @@ import shutil
 import re
 seq = list()
 
-file_folder = "/N/u/tlicknac/Carbonate/Upstream_Test/"   						#Location of upstream sequences
+file_folder = "/N/dc2/scratch/tlicknac/All_Aurelias_Upstream/"                  #Path upstream sequences
 
-seq = os.listdir(file_folder)
+seq = os.listdir(file_folder)                                                   #seq will have list of all file names
 
-print(seq)
-
-for i in seq:
-	real_i = file_folder + i
-        filepath = "/N/u/tlicknac/Carbonate/MEME_Test/" + i                                		#Location of output
+for i in seq:                                                                   #iterate through list of files
+        real_i = file_folder + i
+        filepath = "/N/dc2/scratch/tlicknac/12nt_MEME_Results/" + i     #Location of output
         try:
-            	comm1 = "mkdir " + "/N/u/tlicknac/Carbonate/MEME_Test/" + i                          	
+                comm1 = "mkdir " + "../12nt_MEME_Results/"                              
         except:
-               	pass
+                pass
         os.system(comm1)
-        comm2 = "/N/u/tlicknac/Carbonate/software/meme/install/bin/meme " + real_i + " -dna -oc " + filepath + " -nostatus -time 18000 -maxsize 60000 -nmotifs 5 -minw 5 -maxw 18"   #run my meme, not Carbonate's
+        comm2 = "/N/u/tlicknac/Carbonate/software/meme/install/bin/meme " + real_i + " -dna -oc " + filepath + " -nostatus -time 18000 -maxsize 60000 -nmotifs 5 -minw 6 -maxw 12"   #run my meme
         os.system(comm2)
